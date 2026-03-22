499bb473844b828be2d77f116ec9019a084b6c43
immi16-spec <immimiimran@gmail.com>
2026-03-22 12:46:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import streamlit as st
-import requests
-from PIL import Image
 from rembg import remove
-from io import BytesIO
+from PIL import Image
+import io
 import yt_dlp
 
-st.set_page_config(layout="wide")
 st.title("ProAI Tools")
 
-tab1, tab2, tab3, tab4 = st.tabs(["Thumbnail Downloader", "BG Remover", "Video Downloader", "Image Compressor"])
+tab1, tab2 = st.tabs(["Thumbnail Downloader", "BG Remover"])
 
 with tab1:
-    url = st.text_input("Enter YouTube URL")
-    if st.button("Get Thumbnails"):
+    url = st.text_input("YouTube URL:")
+    if st.button("Get Thumbnail"):
         vid = url.split("v=")[-1].split("&")[0]
         st.image(f"https://img.youtube.com/vi/{vid}/maxresdefault.jpg")
 
@@ -20,22 +18,6 @@ with tab2:
     file = st.file_uploader("Upload Image", type=['png', 'jpg'])
     if file:
         input_image = file.read()
-        output = remove(input_image)
+        output = remove(input_image) # Ab ye chalega!
         st.image(output)
         st.download_button("Download", output, "bg_removed.png")
-
-with tab3:
-    v_url = st.text_input("Paste Video URL")
-    if st.button("Download Video"):
-        ydl_opts = {'outtmpl': 'video.mp4'}
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl: ydl.download([v_url])
-        with open("video.mp4", "rb") as f:
-            st.download_button("Save Video", f, "video.mp4")
-
-with tab4:
-    img_file = st.file_uploader("Upload for Compressor", type=['jpg', 'png'])
-    if img_file:
-        img = Image.open(img_file)
-        buf = BytesIO()
-        img.save(buf, format="WEBP", quality=50)
-        st.download_button("Download Compressed", buf.getvalue(), "image.webp")
